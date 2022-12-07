724ad459103a12abd6e8b2f40df6883256626b9a
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2022-12-07 14:08:21 +0000

    [Add] 5-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  def edit
-  end
 
   def index
     @book = Book.new
@@ -8,11 +6,36 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def create
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+     @book.save
+     redirect_to books_path
+  end
+
+  def edit
+     @book = Book.find(params[:id])
+  end
+
   def show
-    @user =　User.find(params[:id])
+    @user = User.find(params[:id])
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body, :image)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-root to: 'homes#top'
-devise_for :users
-resources :books, only: [:edit, :index, :show]
-resources :users, only: [:edit, :index, :show]
-get "/homes/about" => "homes#about", as: "about"
+  root to: 'homes#top'
+  devise_for :users
+  resources :books, only: [:edit, :index, :show, :create]
+  resources :users, only: [:edit, :index, :show]
+  get "/homes/about" => "homes#about", as: "about"
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
